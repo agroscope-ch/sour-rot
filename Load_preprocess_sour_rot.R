2d7b2fb2dec656f64f2686341252f0324cf282ae
Wilhelm Matthieu <matthieu.wilhelm@agroscope.admin.ch>
2024-06-11 07:04:13 +0200

    adding .csv file and instruction in the readme; commented Load_preprocess_sour_rot.R

--- Load_preprocess_sour_rot.R
+++ Load_preprocess_sour_rot.R
@@ -6,22 +6,17 @@ library(stringr)
 LoadSourRot <- function(normalization = FALSE )
 {
   
-  
+  # normalization: if TRUE, returns the proportion of species in each sample and not the absolute number of occurrences
   dfSourRot <- read_excel(path = "Data/data_sour_rot.xlsx", col_names = T, skip = 1)
   apply(dfSourRot, 2, function(z)all(is.na(z))) |> which()
-  dfSourRot <- dfSourRot |> 
-    dplyr::rename(species = "genotype", order = "ordre")
+  # dfSourRot <- dfSourRot |> 
+  #   dplyr::rename(species = "genotype", order = "ordre")
+  # We remove this problematic sample
   dfSourRot <- dfSourRot |> 
     select(-G4B1) 
   dfSourRot$family[is.na(dfSourRot$family)] <- "Incertae sedis"
   dfSourRot <- dfSourRot |> relocate(order, family, genus, species)
-  
-  # check some of the taxons that have been submitted to Valerie for double check
-  # check_Valerie <- with(dfSourRot, 
-  #                       c(which(stringr::str_detect(species, pattern = "Curvibasidium|Dipodascaceae|fungal|Metschnikowia|Pezizales|Sordariomycetes"))))
-  
-  # replace the NAs in the counts columns by 0
-  # browser()
+
   dfSourRot <- dfSourRot |> 
     mutate(dplyr::across(.cols = -c(order, family, genus, species), .fns = ~replace_na(.,0))) 
   
@@ -40,7 +35,7 @@ LoadSourRot <- function(normalization = FALSE )
   dfSourRot <- dfSourRot |> 
     group_by(order, family, genus, species) |> 
     dplyr::summarise(across(everything(),  ~ sum(.x, na.rm = TRUE))) |> 
-    ungroup() 
+    ungroup()
   if(normalization)
   {
     dfSourRot <- dfSourRot |> 
@@ -49,11 +44,15 @@ LoadSourRot <- function(normalization = FALSE )
   ## removing unwanted spaces at the beggining of a string
   dfSourRot[,1:4] <- apply(dfSourRot[,1:4], 2, function(z) stringr::str_replace(string = z, pattern = "^\\s+", replacement = ""))
   dfSourRot[,1:4] <- apply(dfSourRot[,1:4], 2, function(z) stringr::str_replace(string = z, pattern = "\\s+", replacement = " "))
+  
+  ## vars is the names of the samples. There are indicated as the columns of type "double"
   vars <- colnames(dfSourRot)[sapply(dfSourRot, typeof) =="double"]
+  ## For each sample, we deduce from its name what is its type ("insect", "extern" or "sour rot")
+  ## The patterns are provided as regular expressions
   types <- rep(NA,length(vars))
   pat <- c("male|female|other", 
            "[A-Z]\\d{2}C", # Maj-2digits-C
-           "[A-Z]\\d+?B") #Maj-aussi peu de digits que possible-B
+           "[A-Z]\\d+?B") # Maj-aussi peu de digits que possible-B
   typesNames <- c("insect", "extern", "sour rot")
   cbind(pat, typesNames)
   for(i in 1:length(pat))
@@ -61,6 +60,10 @@ LoadSourRot <- function(normalization = FALSE )
     ind <- grepl(pattern = pat[i], x = vars)
     types[ind] <- typesNames[i]
   }
+  ## A list of three components is returned
+  ## 1: the dataset itself
+  ## 2: the name of the samples
+  ## 3: the types corresponding to the samples
   return(list(dfSourRot = dfSourRot, vars = vars, types = types))
 }
 
--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Fungal communities were studied in 4 different environments:
 
 The aim is to see if there are any differences in terms of fungal community. We have discarded the data of the inside of healthy berries as there was just a very few fungi inside. 
 
-For each observation (i.e. a healthy or infected berry or a _Drosophila_) $`\mathbf{x}_i,\quad i = 1,\dots,n`$, we observe a certain number of different fungi. In concrete terms, let $`p`$ be the number of different fungi observed in all samples, and let $`x_{ik}`$ be the number of occurrences of fungus $`k, \quad k = 1,\dots,p`$ in sample $`i`$. The design matrix $\mathbf{X}$ with generic entries $x{ik}$ is the data set provided in the file [Missing link]()
+For each observation (i.e. a healthy or infected berry or a _Drosophila_) $`\mathbf{x}_i,\quad i = 1,\dots,n`$, we observe a certain number of different fungi. In concrete terms, let $`p`$ be the number of different fungi observed in all samples, and let $`x_{ik}`$ be the number of occurrences of fungus $`k, \quad k = 1,\dots,p`$ in sample $`i`$. The design matrix $`\mathbf{X}`$ with generic entries $`x_{ik}`$ is the data set provided in the file [Missing link]()
 
 ## Data description
 
@@ -21,5 +21,23 @@ The dataset contains 124 samples, distributed as follows :
 
 For each detected fungus, the species, the genus, the family and the order are given. Some are classified as _Incertae sedis_. 
 
+## Note on data saving
 
+To ensure long term support and readability of the dataset, we have saved a copy of the original `.xlsx` file in a `.csv` file. To do so, we have run the following R line of code (using the package [`readxl`](https://readxl.tidyverse.org/))
 
+```
+dfSourRot <- read_excel(path = "Data/data_sour_rot.xlsx", col_names = T, skip = 1)
+write_csv(dfSourRot, file = "Data/data_sour_rot.csv")
+```
+
+In the function `LoadSourRot` in the file [`Load_preprocess_sour_rot.R`](Load_preprocess_sour_rot.R), one can substitute the line 
+```
+dfSourRot <- read_excel(path = "Data/data_sour_rot.xlsx", col_names = T, skip = 1)
+```
+with the line
+
+```
+dfSourRot <- read.csv(file = "Data/data_sour_rot.csv")
+```
+
+to get the exact same result, but relying on the `.csv` file. 
